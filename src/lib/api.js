fd639938a93d6cf74b594bd3a0f0a636abf5e62d
Tucker Bradford <github@tuckerbradford.com>
2019-10-27 18:30:41 +0100

    Initial

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,1 +1,7 @@
-export default () => {};
+export default () => {
+  const boats = () => {
+    let response = async fetch("https://staging.farkwar.com/boats")
+    return await response.json
+  }
+};
+
